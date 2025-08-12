7177c4d275b706b4705f082bd1a1cb4a5a621cb1
Deepanshi Shukla <shuklashiri154@gmail.com>
2025-08-12 22:00:51 +0530

    Create identify vowel.c

---
+++ identify vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter any alphabet");
+scanf("%ch",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("alphabet is vowel");
+}
+else
+{
+printf("alphabet is constant");
+}
+return 0;
+}
